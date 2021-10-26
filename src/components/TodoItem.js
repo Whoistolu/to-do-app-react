f5f4798ab63d9144ecfc0380f8ba6825c7b0a8ec
Whoistolu <digittolu25@gmail.com>
2021-10-26 06:30:51 +0100

    Update the input checkbox to be a controlled input

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,16 @@ import React from "react"
 
 class TodoItem extends React.Component {
   render() {
-    return <li>{this.props.todo.title}</li>
+    return (
+      <li>
+          <input
+            type="checkbox"
+            checked={this.props.todo.completed}
+            onChange={() => console.log("clicked")}
+          />
+          {this.props.todo.title}
+      </li>
+    )
   }
 }
 
