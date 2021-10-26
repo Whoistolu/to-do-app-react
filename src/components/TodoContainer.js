8be81cf0ab6a1031b7bbf5d50389a79cd8dec7a4
Whoistolu <digittolu25@gmail.com>
2021-10-26 05:17:44 +0100

    Add a state to the todocontainer.js

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,26 @@
 import React from "react"
 class TodoContainer extends React.Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+   };
+
   render() {
     return (
       <React.Fragment>
