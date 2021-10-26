de99e941334f93fa7683760027db5ae863d801c2
Whoistolu <digittolu25@gmail.com>
2021-10-26 05:43:57 +0100

    Update the todoitem.js file

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import TodosList from "./TodosList";
 class TodoContainer extends React.Component {
 
   state = {
@@ -23,11 +24,9 @@ class TodoContainer extends React.Component {
 
   render() {
     return (
-      <ul>
-      {this.state.todos.map(todo => (
-        <li>{todo.title}</li>
-      ))}
-    </ul>
+      <div>
+        <TodosList todos={this.state.todos} />
+      </div>
     )
   }
 }
