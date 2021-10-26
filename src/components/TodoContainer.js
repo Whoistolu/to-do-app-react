16fc0c9f04c21274961dfd9e00087c17fadeb99d
Whoistolu <digittolu25@gmail.com>
2021-10-26 09:10:05 +0100

    Update the state using the setState() method

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -24,8 +24,15 @@ class TodoContainer extends React.Component {
     ]
    };
 
-   handleChange = (id) => {
-    console.log("clicked", id);
+   handleChange = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
   };
 
   render() {
