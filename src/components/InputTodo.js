284226c39b292ed490c7f2c07fa6c07556c051f9
Whoistolu <digittolu25@gmail.com>
2021-10-26 11:29:37 +0100

    Add inline styles

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -24,16 +24,17 @@ class InputTodo extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
           type="text"
+          className="input-text"
           placeholder="Add todo..."
           value={this.state.title}
           name="title"
           onChange={this.onChange}
         />
-        <button>Submit</button>
-      </form>
+  <button className="input-submit">Submit</button>
+</form>
     )
   }
 }
