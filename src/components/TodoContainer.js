6bbddd588ecd97726b326c3c86d4d163a881348a
Whoistolu <digittolu25@gmail.com>
2021-10-26 06:06:01 +0100

    Update the header.js file

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,7 @@
 import React from "react"
 import TodosList from "./TodosList";
+import Header from "./Header"
+
 class TodoContainer extends React.Component {
 
   state = {
@@ -25,6 +27,7 @@ class TodoContainer extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <TodosList todos={this.state.todos} />
       </div>
     )
