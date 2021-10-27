a1427db13a372ac82ae35b34f81a68fa9a106547
Whoistolu <digittolu25@gmail.com>
2021-10-27 04:39:24 +0100

    Assign save todos directly to the initial state

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from "uuid"
 
 
 const TodoContainer = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(getInitialTodos())
 
   const handleChange = id => {
     setTodos(prevState =>
@@ -50,17 +50,13 @@ const TodoContainer = () => {
     )
   }
 
-  useEffect(() => {
-    console.log("test run")
-  
+
+  function getInitialTodos() {
     // getting stored items
     const temp = localStorage.getItem("todos")
-    const loadedTodos = JSON.parse(temp)
-  
-    if (loadedTodos) {
-      setTodos(loadedTodos)
-    }
-  }, [])
+    const savedTodos = JSON.parse(temp)
+    return savedTodos || []
+  }
 
   useEffect(() => {
     // storing todos items
